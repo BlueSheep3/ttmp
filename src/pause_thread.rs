982f677e01db3c627c85a13a93d3e91ad1707f9f
Per6 <per.daude@t-online.de>
2024-06-24 19:23:25 +0200

    mute also pauses

--- src/pause_thread.rs
+++ src/pause_thread.rs
@@ -1,13 +1,18 @@
 use std::sync::mpsc::Sender;
 use std::thread;
 use std::time::Duration;
-use winapi::um::winuser::{GetAsyncKeyState as get_async_key_state, VK_MEDIA_PLAY_PAUSE};
+use winapi::um::winuser::{
+	GetAsyncKeyState as get_async_key_state, VK_MEDIA_PLAY_PAUSE, VK_VOLUME_MUTE,
+};
 
 pub fn main(sender: &Sender<String>) -> ! {
 	let mut pause_pressed = false;
 	loop {
 		// SAFETY: accesses global keyboard state
-		if unsafe { get_async_key_state(VK_MEDIA_PLAY_PAUSE) } as u16 & 0x8000 != 0 {
+		let play_pause_state =
+			unsafe { get_async_key_state(VK_MEDIA_PLAY_PAUSE) } as u16 & 0x8000 != 0;
+		let mute_state = unsafe { get_async_key_state(VK_VOLUME_MUTE) } as u16 & 0x8000 != 0;
+		if play_pause_state || mute_state {
 			if !pause_pressed {
 				pause_pressed = true;
 				sender
